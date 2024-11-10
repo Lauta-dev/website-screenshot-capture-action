78ea8ddd76810b43eca570e57309c62fa8efdb62
lauta-dev <altairdiaz245@gmail.com>
2024-11-10 13:20:07 -0300

    asd

--- yamlToObject.ts
+++ yamlToObject.ts
@@ -3,6 +3,8 @@ import { readFileSync } from "node:fs";
 import { PageFile } from "./interface/pageFile";
 
 export async function yamlToObject(file: string): Promise<PageFile[] | Error> {
+	console.log(file);
+
 	try {
 		const yaml = readFileSync(file, "utf8");
 		return parse(yaml).pages;

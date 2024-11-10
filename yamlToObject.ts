6dc48ac07601c0732f258629d2a491f114d5a7f2
lauta-dev <altairdiaz245@gmail.com>
2024-11-10 13:26:06 -0300

    asd

--- yamlToObject.ts
+++ yamlToObject.ts
@@ -6,6 +6,11 @@ export async function yamlToObject(file: string): Promise<PageFile[] | Error> {
 	try {
 		const yaml = readFileSync(file, "utf8");
 		console.log(JSON.stringify(yaml));
+		const parseYaml = parse(yaml);
+
+		console.log(parseYaml);
+		throw new Error("asd");
+
 		return parse(yaml).pages;
 	} catch (error) {
 		throw new Error(error as string);

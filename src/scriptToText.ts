05412036c42cbf25fe2d93a2380628a7724b23fa
lauta-dev <altairdiaz245@gmail.com>
2024-11-14 11:16:32 -0300

    script is '' per default

--- src/scriptToText.ts
+++ src/scriptToText.ts
@@ -3,6 +3,11 @@ import fs from "node:fs";
 export function scriptToText(script?: string) {
 	console.log(`::debug::scriptToText: ${script}`);
 
+	if (!script) {
+		console.log(`::warning::No se especificó el script, se ignorará`);
+		return "";
+	}
+
 	if (!script.endsWith(".js")) {
 		console.log(`::warning::El script no es un archivo JS, se ignorará`);
 		return "";

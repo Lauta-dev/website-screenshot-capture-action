06c8c80581e15589ca380de3a0ab42141e4a346e
lauta-dev <altairdiaz245@gmail.com>
2024-11-14 11:13:35 -0300

    script now is ' ' per default

--- src/scriptToText.ts
+++ src/scriptToText.ts
@@ -3,10 +3,6 @@ import fs from "node:fs";
 export function scriptToText(script?: string) {
 	console.log(`::debug::scriptToText: ${script}`);
 
-	if (!script) {
-		return "";
-	}
-
 	if (!script.endsWith(".js")) {
 		console.log(`::warning::El script no es un archivo JS, se ignorará`);
 		return "";
